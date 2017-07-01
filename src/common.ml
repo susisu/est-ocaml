c59c864d8a29b7bf4fec119b259ca2c5477edaaf
Susisu <susisu2413@yahoo.co.jp>
2017-07-01 12:46:45 +0900

    Fix column number was wrong

--- src/common.ml
+++ src/common.ml
@@ -14,7 +14,7 @@ module Position = struct
   let to_string p =
     let fname = p.pos_fname in
     let lnum = p.pos_lnum in
-    let cnum = p.pos_cnum + 1 in
+    let cnum = p.pos_cnum - p.pos_bol + 1 in
     let fname_prefix =
       if String.is_empty fname then ""
       else "\"" ^ fname ^ "\""
